af45841b4524e48de9912d6a2617ec3a6380efb1
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2025-02-19 01:27:28 +0300

    feat: keybindings

---
+++ src/core/keybinding.zig
@@ -0,0 +1,9 @@
+const rl = @import("raylib");
+
+pub const moveForward = rl.KeyboardKey.w;
+pub const moveBackward = rl.KeyboardKey.s;
+pub const moveLeft = rl.KeyboardKey.a;
+pub const moveRight = rl.KeyboardKey.d;
+pub const jump = rl.KeyboardKey.space;
+pub const sneak = rl.KeyboardKey.left_shift;
+pub const sprint = rl.KeyboardKey.left_control;
